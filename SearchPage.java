b28de1ac3fb827b6e07ab741a7b608e1bde5d0a2
Soroush.Esk <semp1381@gmail.com>
2022-06-26 21:46:48 +0430

    update SearchPage.java and Main.java

--- SearchPage.java
+++ SearchPage.java
@@ -1,8 +1,10 @@
 public class SearchPage{
-	int soroush;
+	int soroush = 0;
 	public String name;
 	public SearchPage(String soroush){
 	this.name = soroush;
 	}
-
+	public String toString(){
+		return "soroush :" + soroush + " name : " name;
+	}
 }
